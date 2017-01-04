c385a39d0fbe20700e4920ab481823e53cd353c7
TingWei <jjohny51207@gmail.com>
2017-01-04 14:42:25 +0800

    add string

---
+++ abc/abc/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "xcvxcv12";
+            Console.WriteLine(s.Length);
+            Console.WriteLine(s);
+        }
+    }
+}
